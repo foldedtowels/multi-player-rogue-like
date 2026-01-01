3f167a00e5012eec64edc897948bec0a1c0cfe8c
foldedtowels <benjamin.lcuckman@gmail.com>
2026-01-01 18:10:42 -0500

    Add multiplayer testing batch file for rapid iteration

---
+++ test_multiplayer.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Launching 3 instances for multiplayer testing...
+
+REM Update this path to your Godot executable
+set GODOT_PATH="C:\Users\benja\Desktop\Godot_v4.5.1-stable_win64.exe"
+
+REM Launch 3 instances with 1 second delay between each
+start "Game Instance 1" %GODOT_PATH% --path "%~dp0"
+timeout /t 1 /nobreak >nul
+
+start "Game Instance 2" %GODOT_PATH% --path "%~dp0"
+timeout /t 1 /nobreak >nul
+
+start "Game Instance 3" %GODOT_PATH% --path "%~dp0"
+
+echo All instances launched!
+echo.
+echo Instructions:
+echo 1. Instance 1: Click Multiplayer -^> Host Game
+echo 2. Instance 2-3: Click Multiplayer -^> Join Game (127.0.0.1)
+echo 3. Instance 1: Click Start Game when 3/3 players
+echo.
+pause
